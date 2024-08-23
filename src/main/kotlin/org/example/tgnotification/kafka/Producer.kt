7d079c9ea05a719113412f90900f0d26b2652347
Tom Tam <tom.tam@fwd.com>
2024-08-23 20:25:30 +0800

    Init

---
+++ src/main/kotlin/org/example/tgnotification/kafka/Producer.kt
@@ -0,0 +1,4 @@
+package org.example.tgnotification.kafka
+
+class Producer {
+}
---
+++ src/test/kotlin/org/example/tgnotification/TgNotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.tgnotification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TgNotificationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
